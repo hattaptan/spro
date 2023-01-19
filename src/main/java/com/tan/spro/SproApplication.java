0f5f47cb476b57c165cc8df53693a5f49d59e299
Hattap TAN <47hattaptan@gmail.com>
2023-01-19 16:41:10 +0300

    Initial commit

---
+++ src/main/java/com/tan/spro/SproApplication.java
@@ -0,0 +1,13 @@
+package com.tan.spro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tan/spro/SproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tan.spro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
